14a425bfd9a8751525fae5ce95a808be1e876ce1
Samuel Martin <samination@gmail.com>
2021-04-06 20:18:53 -0400

    Initial commit

---
+++ src/com/concordia/java/sam/Main.java
@@ -0,0 +1,10 @@
+package com.concordia.java.sam;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        System.out.println("World");
+    }
+}
