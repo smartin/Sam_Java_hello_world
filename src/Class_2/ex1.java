18e0a75414772942a21649d92db54f54bb0ecd37
Samuel Martin <samination@gmail.com>
2021-04-08 19:27:56 -0400

    Added three new classes of notes and examples Data types Identifier conventions Cases Analyst thinking for efficiency, manageability, scalability

---
+++ src/Class_2/ex1.java
@@ -0,0 +1,30 @@
+package Class_2;
+
+public class ex1 {
+
+    public static void main(String[] args) {
+
+        boolean taxable = true; // true or false
+
+        char taxCode = 'C'; // must be between single quotes
+        String productDescription = "Circular Saw"; // must be between double quotes
+
+// Floating point numbers
+    double tempC2 = 19.3;
+    float tempC = 19.3f; // Do not use it for money because it keeps crazy decimals that trick you up when using time and big numbers
+
+ // Integer types
+    byte counter = 1;
+    short counter2 = 2;
+    int counter3 = 4;
+    long population = 12345678;
+    // decimal ---> the correct integer type for money use
+
+    }
+
+
+
+
+
+
+}
---
+++ src/Class_2/ex2.java
@@ -0,0 +1,17 @@
+package Class_2;
+
+public class ex2 {
+
+    public static void main(String[] args) {
+
+        float x = 0.3f;
+        float y = 0.1f;
+
+        float z = x * y;
+
+        System.out.println(z);
+
+
+    }
+
+}
